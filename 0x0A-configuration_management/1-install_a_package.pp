9285bf85316dc680216016276a2e48d2379d8df6
17070610 <schimea@hotmail.com>
2025-01-10 03:10:46 -0800

    Install a pckage

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Install a package
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+#!/usr/bin/puppet
+
+# Install flask of version 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
