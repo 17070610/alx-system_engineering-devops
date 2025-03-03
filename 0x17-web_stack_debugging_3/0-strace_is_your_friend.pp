3dbb768d6f2cf74fff124df62b62ad3c4f2d5df1
17070610 <schimea@hotmail.com>
2025-03-02 20:07:05 -0800

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,12 @@
-# automated puppet fix (to find out why Apache is returning a 500 error)
+# Automated Puppet fix for Apache 500 error on WordPress site
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+exec { 'Fix WordPress site':
+  command  => 'sudo sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   provider => shell,
+}
+
+exec { 'Restart Apache':
+  command  => 'sudo systemctl restart apache2',
+  provider => shell,
+  require  => Exec['Fix WordPress site'],
 }
